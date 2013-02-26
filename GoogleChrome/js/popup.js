047d348a74a0ccda2f61b218e80edfa24c9b0f37
northerneyes <gbuhanov@gmail.com>
2013-02-26 23:15:08 +0600

    Add jsRender framework for template list.

--- GoogleChrome/js/popup.js
+++ GoogleChrome/js/popup.js
@@ -5,3 +5,19 @@
  * Time: 21:32
  * To change this template use File | Settings | File Templates.
  */
+
+$(function() {
+
+    // Render the template with the tabs data and insert
+    // the rendered HTML under the "tabsList" element
+    $("#tabsList" ).html(
+        $( "#tabsTemplate" ).render(tabs)
+    );
+
+});
+
+//var tabs = [
+//    { title: "The Red Violin", time: "1998" },
+//    { title: "Eyes Wide Shut", time: "1999" },
+//    { title: "The Inheritance", time: "1976" }
+//];
