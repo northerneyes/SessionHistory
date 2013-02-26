13474d55ee7dfa28802c94e7485b65d45bf2fd3f
northerneyes <gbuhanov@gmail.com>
2013-02-26 23:17:15 +0600

    fixes)

---
+++ GoogleChrome/js/background.js
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: George
+ * Date: 26.02.13
+ * Time: 22:03
+ * To change this template use File | Settings | File Templates.
+ */
+var tabs = [
+    { title: "The Red Violin", time: "1998" },
+    { title: "Eyes Wide Shut", time: "1999" },
+    { title: "The Inheritance", time: "1976" }
+];
